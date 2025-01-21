aadf337cab2e50509f627572c9c8ec841cf869f9
Ikekano <30634494+Ikekano@users.noreply.github.com>
2025-01-21 16:12:22 -0600

    Initial commit

---
+++ run.bat
@@ -0,0 +1,82 @@
+@echo off
+
+:Menu
+cls
+echo.
+echo YT-DLP Youtube Downloader 
+echo.
+echo 1. Download Video (MP4)
+echo 2. Download Audio (MP3)
+echo 3. Download in Specified Format
+echo 4. Download Thumbnail
+echo 5. Exit 
+echo.
+SET /P M=Select Option: 
+IF /I %M%==1 GOTO YTDL4
+IF /I %M%==2 GOTO YTDL3
+IF /I %M%==3 GOTO YTDLC
+IF /I %M%==4 GOTO YTDLT
+IF /I %M%==5 GOTO EOF
+
+echo.
+echo Error: Invalid Input
+echo.
+echo [Press Any Key To Continue]
+pause > nul
+GOTO MENU 
+
+:YTDL4
+cls
+echo.
+echo YT-DLP Youtube Downloader (MP4)
+echo.
+SET /P URL=Enter URL: 
+echo.
+yt-dlp.exe --remux-video "mp4" --ffmpeg-location "%~dp0\ffmpeg\bin" "%URL%" 
+::--paths "%~dp0../Downloads"
+timeout /t 10 /nobreak
+GOTO Menu
+
+:YTDL3
+cls
+echo.
+echo YT-DLP Youtube Downloader (MP3)
+echo.
+SET /P URL=Enter URL: 
+echo.
+SET /P Q=Desired Audio Quality (0 Best - 10 Worst): 
+echo.
+yt-dlp.exe -x --audio-format "mp3" --audio-quality %Q% --ffmpeg-location "%~dp0\ffmpeg\bin" "%URL%"
+timeout /t 5 /nobreak
+GOTO Menu
+
+:YTDLC
+cls
+echo.
+echo YT-DLP Youtube Downloader 
+echo.
+SET /P URL=Enter URL: 
+echo.
+yt-dlp.exe --list-formats "%URL%"
+echo.
+SET /P F=Desired Output Format: 
+echo.
+yt-dlp.exe -f %F% --ffmpeg-location "%~dp0\ffmpeg\bin" "%URL%"
+timeout /t 5 /nobreak
+GOTO Menu
+
+:YTDLT
+cls
+echo.
+echo YT-DLP Youtube Downloader 
+echo.
+SET /P URL=Enter URL: 
+echo.
+yt-dlp.exe --list-thumbnails "%URL%"
+echo.
+echo.
+SET /P T=Desired Thumbnail: 
+echo.
+yt-dlp.exe  "%URL%"
+timeout /t 5 /nobreak
+GOTO Menu
