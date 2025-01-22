3585f1d85a414c1e6f812ec065f82d2588100af6
Ikekano <30634494+Ikekano@users.noreply.github.com>
2025-01-21 20:20:24 -0600

    Thumbnail overhaul
    Overhaul thumbnail function, add comment headers, and update to new menu return method

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
 @echo off
 
+::
+::		Main Menu
+::
 :Menu
 cls
 echo.
@@ -7,8 +10,8 @@ echo YT-DLP Youtube Downloader
 echo.
 echo 1. Download Video (MP4)
 echo 2. Download Audio (MP3)
-echo 3. Download in Specified Format
-echo 4. Download Thumbnail
+echo 3. Download Video in Specified Format
+echo 4. Download Video Thumbnail
 echo 5. Exit 
 echo.
 SET /P M=Select Option: 
@@ -23,8 +26,11 @@ echo Error: Invalid Input
 echo.
 echo [Press Any Key To Continue]
 pause > nul
-GOTO MENU 
+GOTO Menu
 
+::
+:: 		Download MP4
+::
 :YTDL4
 cls
 echo.
@@ -35,9 +41,14 @@ echo.
 IF not exist "%~dp0\Downloads" mkdir %~dp0\Downloads
 yt-dlp.exe --remux-video "mp4" --embed-thumbnail --ffmpeg-location "%~dp0\ffmpeg\bin" --paths "%~dp0\Downloads" "%URL%" 
 ::--paths "%~dp0../Downloads"
-timeout /t 5 /nobreak
+echo.
+echo Returning to Main Menu...
+timeout /t 5 /nobreak > nul
 GOTO Menu
 
+::
+:: 		Download MP3
+::
 :YTDL3
 cls
 echo.
@@ -49,9 +60,14 @@ SET /P Q=Desired Audio Quality (0 Best - 10 Worst):
 echo.
 IF not exist "%~dp0\Downloads" mkdir %~dp0\Downloads
 yt-dlp.exe -x --audio-format "mp3" --embed-thumbnail --audio-quality %Q% --ffmpeg-location "%~dp0\ffmpeg\bin" --paths "%~dp0\Downloads" "%URL%"
-timeout /t 5 /nobreak
+echo.
+echo Returning to Main Menu...
+timeout /t 5 /nobreak > nul
 GOTO Menu
 
+::
+:: 		Download With Chosen Format
+::
 :YTDLC
 cls
 echo.
@@ -65,9 +81,14 @@ SET /P F=Desired Output Format:
 echo.
 IF not exist "%~dp0\Downloads" mkdir %~dp0\Downloads
 yt-dlp.exe -f %F% --embed-thumbnail --ffmpeg-location "%~dp0\ffmpeg\bin" --paths "%~dp0\Downloads" "%URL%"
-timeout /t 5 /nobreak
+echo.
+echo Returning to Main Menu...
+timeout /t 5 /nobreak > nul
 GOTO Menu
 
+::
+:: 		Download Thumbnail
+::
 :YTDLT
 cls
 echo.
@@ -75,8 +96,39 @@ echo YT-DLP Youtube Thumbnail Downloader
 echo.
 SET /P URL=Enter URL: 
 echo.
+GOTO YTDLTP
+
+:YTDLTP
+cls
+echo.
+echo YT-DLP Youtube Thumbnail Downloader 
+echo.
+echo Enter URL: %URL%
+echo.
+SET /P T=Download Highest Quality Thumbnail? [y/n]:
+IF %T%==y GOTO YTDLT1
+IF %T%==n GOTO YTDLT2
+echo.
+echo Error: Invalid Input
+echo.
+echo [Press Any Key To Continue]
+pause > nul
+GOTO YTDLTP 
+
+:YTDLT1
+echo.
 IF not exist "%~dp0\Downloads" mkdir %~dp0\Downloads
-yt-dlp.exe --skip-download --list-thumbnails --convert-thumbnails "png" --write-thumbnail --ffmpeg-location "%~dp0\ffmpeg\bin" --paths "%~dp0\Downloads" "%URL%"
+yt-dlp.exe --skip-download --convert-thumbnails "png" --write-thumbnail --ffmpeg-location "%~dp0\ffmpeg\bin" --paths "%~dp0\Downloads" "%URL%"
+echo.
+echo Returning to Main Menu...
+timeout /t 5 /nobreak>nul
+GOTO Menu
+
+:YTDLT2
+echo.
+yt-dlp.exe --list-thumbnails "%URL%"
+echo.
+echo [Ctrl + Left-Click] A Link Above to Select Specific Thumbnail to Manually Download from Web
 echo.
 echo [Press Any Key To Continue]
 pause > nul
